77b4dd4bcdbe5f1f1d78a0014e83b5e67735d93e
triendo <trien.do@ndm.ox.ac.uk>
2018-11-03 12:54:13 +0000

    Load maps ok

---
+++ WorldData/RemoteWebservices/WorldBankWebservices.cs
@@ -0,0 +1,87 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Web;
+using System.Diagnostics;
+
+namespace WorldData.RemoteWebservices
+{
+    public class WorldBankWebservices
+    {
+        //https://datahelpdesk.worldbank.org/knowledgebase/articles/898581-api-basic-call-structure
+        public static string WORLDBANK_ROOT = ConfigurationManager.AppSettings["WorldBankEndPoint"];
+        public static string API_SAMPLE_LIST = "sampleListFile";
+        public static string API_SAMPLE_COLLECT = "collectSamples";
+        public static string API_SAMPLE_COLLECT_STATUS = "bamCollectionStatus";
+        public static string API_SAMPLE_UPLOAD = "uploadBamToNCBI";
+        public static string API_SAMPLE_UPLOAD_STATUS = "uploadBamToNCBIStatus";
+        public static string API_REFERENCE = "reference/"; 
+        public static JObject getResource(string endpoint)
+        {
+            try
+            {
+                var client = new HttpClient();
+                client.BaseAddress = new Uri(WORLDBANK_ROOT);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = client.GetAsync(endpoint).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    var data = response.Content.ReadAsStringAsync().Result;
+                    JObject rs = (JObject)JsonConvert.DeserializeObject(data);
+                    return rs;
+                }
+                else
+                    return JObject.Parse(WorldBankWebservices.getJsonResultString("1", response.ToString()));
+            }
+            catch (Exception e)
+            {
+                return JObject.Parse(WorldBankWebservices.getJsonResultString("1", e.Message));
+            }
+        }
+
+        public static JObject postResource(string endpoint, string jsonString)
+        {
+            try
+            {
+                var jsonData = new StringContent(jsonString, Encoding.UTF8, "application/json");
+                var client = new HttpClient();
+                client.BaseAddress = new Uri(WORLDBANK_ROOT);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.Timeout = new TimeSpan(0, 45, 30);//0 hours, 45 minutes, 30 seconds
+                HttpResponseMessage response = client.PostAsync(endpoint, jsonData).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    var data = response.Content.ReadAsStringAsync().Result;
+                    JObject rs = (JObject)JsonConvert.DeserializeObject(data);
+                    return rs;
+                }
+                else
+                    return JObject.Parse(WorldBankWebservices.getJsonResultString("1", response.ToString()));
+            }
+            catch (Exception e)
+            {
+                return JObject.Parse(WorldBankWebservices.getJsonResultString("1", e.Message));
+            }
+        } 
+    }
+
+    public static string getJsonResultString(string retCode, string value, string note = ""){
+        var keyValues = new Dictionary<string, string>
+        {
+            { "status", retCode},
+            { "data", value},
+            { "note", note}                    
+        };
+        var serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
+        serializer.MaxJsonLength = Int32.MaxValue;
+        string ret = serializer.Serialize(keyValues); 
+        return ret;
+    }
+}
---
+++ WorldData/Scripts/app/maps.js
@@ -0,0 +1,94 @@
+﻿var myGeoJSONPath = '../Data/custom.geojson';
+var myCustomStyle = {
+    stroke: false,
+    fill: true,
+    fillColor: '#fff',
+    fillOpacity: 1
+}
+$.getJSON(myGeoJSONPath, function (json) {
+    var map = L.map('map').setView([39.74739, -105], 4);
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors | Demo app ' +
+			'<a mailto="vantriendo@hotmail.com">&copy Trien Do</a>',
+        id: 'mapbox.streets'
+    }).addTo(map);
+    map.fitWorld();
+    
+    /*L.geoJson(json, {
+        clickable: false,
+        style: myCustomStyle
+    }).addTo(map);
+    */
+    var allowedCountries = {};
+    L.geoJson(json, {
+        clickable: true,
+        style: function (item) {
+            if (item.properties.type == 'stateline') {
+                return {
+                    fill: false,
+                    stroke: true,
+                    color: '#EAEAEA',
+                    weight: 2
+                }
+            } if (item.geometry.type == 'Point') {
+                if (item.properties.importance > 1) {
+                    return {
+                        fill: false,
+                        stroke: false
+                    }
+                }
+
+                return {
+                    fill: true,
+                    fillOpacity: 1,
+                    stroke: false,
+                    fillColor: "#aaa",
+                    radius: 2 / item.properties.importance
+                }
+
+            } else {
+                return {
+                    fillColor: '#fff',
+                    fillOpacity: 0.7,
+                    fill: true,
+                    color: '#eeeeff',
+                    weight: 1
+                }
+            }
+        },
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng)
+                .bindLabel(feature.properties.name, {
+                    noHide: true
+                });
+        },
+        onEachFeature: function (feature, layer) {
+            var name = feature.properties.name;
+            console.log(feature.properties.sov_a3);
+            function ctxFillColor() {
+                return allowedCountries[name] ? '#ffddff' : '#fff';
+            }
+            layer.on('click', function () {
+                allowedCountries[name] = !allowedCountries[name];
+                console.log(allowedCountries[name])
+                layer.setStyle({
+                    fillColor: ctxFillColor()
+                });
+            });
+
+            layer.on('mouseover', function () {
+                layer.setStyle({
+                    fillColor: '#ffaaff'
+                })
+            })
+
+            layer.on('mouseout', function () {
+                layer.setStyle({
+                    fillColor: ctxFillColor()
+                })
+            })
+        }
+    }).addTo(map);
+
+})
